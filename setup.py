9cad7dd44fe50b0de005a277c0b1fa8f614fe1bd
Kevin <kevin.roj@p8h.de>
2020-01-31 10:12:40 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="logparser-P8H", # Replace with your own username
+    version="0.1.2",
+    author="LogPAI",
+    author_email="",
+    description="TODO",
+    url="https://github.com/P8H/logparser",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
